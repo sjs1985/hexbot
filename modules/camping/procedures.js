c9c33ab08af88076525109a695cd26032822dc89
Guilherme Sá <gresendesa@gmail.com>
2016-05-10 02:26:02 -0300

    -Adding features

--- modules/camping/procedures.js
+++ modules/camping/procedures.js
@@ -37,8 +37,8 @@ camping.procedure("hackAccount", function(shared){
 })
 
 camping.procedure("accessKnownAccount", function(shared){
-	getDOMElement("input", "name", "acc", 0).value = getDOMElement("div", "class", "alert alert-error", 0).value.match(/[0-9]+/)[0]
-	getDOMElement("input", "name", "pass", 0).value = getDOMElement("strong", null, null, 1).childNodes[0].nodeValue. //Fill the password field with the password on screen
+	getDOMElement("input", "name", "acc", 0).value = getDOMElement("div", "class", "alert alert-error", 0).innerHTML.match(/[0-9]+/)[0]
+	getDOMElement("input", "name", "pass", 0).value = getDOMElement("strong", null, null, 1).childNodes[0].nodeValue //Fill the password field with the password on screen
 	getDOMElement("input", "type", "submit", 1).click() //Click on the Login button
 })
 
@@ -52,25 +52,15 @@ camping.procedure("goToOwnLogTab", function(){
 
 camping.procedure("cleanMyIpClues", function(shared){
 	var textArea = getDOMElement("textarea", "class", "logarea", 0)
-	if (textArea.value.length > 0){
-		shared.isEmpty = false
-		var pattern = new RegExp("^.*" + getMyIp(true) + ".*$")
-		textArea.value = removeLinesFromText(textArea.value, pattern)
-		getDOMElement("input", "class", "btn btn-inverse", "last").click()
-	} else {
-		shared.isEmpty = true
-	}
+	var pattern = new RegExp("^.*" + getMyIp(true) + ".*$")
+	textArea.value = removeLinesFromText(textArea.value, pattern)
+	getDOMElement("input", "class", "btn btn-inverse", "last").click()
 })
 
 camping.procedure("cleanTextAreaContent", function(shared){
 	var textArea = getDOMElement("textarea", "class", "logarea", 0)
-	if (textArea.value.length > 0){
-		shared.isEmpty = false
-		textArea.value = ""
-		getDOMElement("input", "class", "btn btn-inverse", "last").click()
-	} else {
-		shared.isEmpty = true
-	}
+	textArea.value = ""
+	getDOMElement("input", "class", "btn btn-inverse", "last").click()
 })
 
 camping.procedure("submitLogs", function(shared){
@@ -84,14 +74,15 @@ camping.procedure("extractTransferLogAccount", function(shared){
 	var accounts = []
 	var myIpPattern = new RegExp("^.*" + getMyIp(true) + ".*$")
 	for (var i = 0; i < lines.length; i++) {
-		if ((/\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\] transferred \$[0-9]+ from #[0-9]+.*to #[0-9]+ at localhost/.test(lines[i])) &&
+		if (((/\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\] transferred \$[0-9]+ from #[0-9]+.*to #[0-9]+ at localhost/.test(lines[i]))) &&
 			(!myIpPattern.test(lines[i]))) {
 			var result = lines[i].match(/#[0-9]+/g)
-			accounts.push(result[1].replace("#", ""))
+			shared.accounts.push(result[1].replace("#", ""))
 		} else {
 			outputLines.push(lines[i])
 		}
 	}
+	accounts = shared.accounts
 	shared.accounts = accounts.filter(function(value, pos) {
 		return accounts.indexOf(value) == pos
 	})
@@ -105,3 +96,10 @@ camping.procedure("goToTargetLogs", function(){
 camping.procedure("showResult", function(shared){
 	console.log(shared.accounts)
 })
+
+camping.procedure("transferMoneyToTarget", function(shared){
+	getDOMElement("input", "name", "acc", 0).value = shared.myAccount //Fill the To field
+	getDOMElement("input", "name", "ip", 1).value = shared.ip //Fill the Bank IP field
+	getDOMElement("button", "class", "btn btn-success", 0).click() //Click on the Transfer Money button
+})
+
--- modules/camping/sequences/bankCamping.js
+++ modules/camping/sequences/bankCamping.js
@@ -5,15 +5,15 @@ foo.instructions = [
 	{"@startProcess": ["logout", "goToIp", "isThereMessageError", {"gotoif":["*.$", "@abortProcess"]}]},
 	{"@tryHostConnection": ["forceToAccessTarget", "isThereMessageError", {"gotoif":["*.$", "@accessTarget"]}, "hackTargetBruteForce", "isThereMessageError", {"gotoif":["*.$", "@abortProcess"]}, {"wait":"_forPageToReload"}]},
 	{"@accessTarget": "signInKnownTarget"},
-	{"@cleanMyClues": ["extractTransferLogAccount", "cleanMyIpClues", {"gotoif": ["*.isEmpty == true", "@cleanOwnLogs"]}, {"wait": "_forPageToReload"}]},
-	{"@cleanOwnLogs": ["goToOwnLogTab", "cleanTextAreaContent", {"gotoif": ["*.isEmpty == true", "@abortProcess"]}, {"wait": "_forPageToReload"}]},
-	{"@startListener": ["goToTargetLogs", "extractTransferLogAccount", {"gotoif":["*.accounts.length > 0", "@startTransfer"]}, {"wait":2000}, {"gotoif":["true", "@startListener"]}]},
-	{"@startTransfer": ["submitLogs", {"wait": "_forPageToReload"}, "logout", "goToIp", "hackAccount", "isThereMessageError", {"gotoif":["*.$", "@accessKnownAccount"]}, {"wait": "_forPageToReload"}]},
-	{"@accessUnknownAccount": ["accessUnknownAccount", {"gotoif": ["true", "@abortProcess"]}]},
+	{"@cleanMyClues": ["extractTransferLogAccount", "cleanMyIpClues", "isThereMessageError", {"gotoif":["*.$", "@cleanOwnLogs"]}, {"wait": "_forPageToReload"}]},
+	{"@cleanOwnLogs": ["goToOwnLogTab", "cleanTextAreaContent", "isThereMessageError", {"gotoif":["*.$", "@startListener"]}, {"wait": "_forPageToReload"}]},
+	{"@startListener": ["goToTargetLogs", "extractTransferLogAccount", {"gotoif":["*.accounts.length > 0", "@submitChangedLogs"]}, {"wait":2000}, {"gotoif":[1, "@startListener"]}]},
+	{"@submitChangedLogs": ["submitLogs", "isThereMessageError", {"gotoif":["*.$", "@startTransfer"]}, {"wait": "_forPageToReload"}]},
+	{"@startTransfer": ["logout", "goToIp", "hackAccount", "isThereMessageError", {"gotoif":["*.$", "@accessKnownAccount"]}, {"wait": "_forPageToReload"}]},
+	{"@accessUnknownAccount": ["accessUnknownAccount", {"gotoif": [1, "@trasferMoney"]}]},
 	{"@accessKnownAccount": "accessKnownAccount"},
+	{"@trasferMoney": ["isThereMessageError", {"gotoif":["*.$", "@cleanOwnLogsAgain"]}, {"wait": 1000}, "transferMoneyToTarget"]},
+	{"@cleanMyTransferLog": ["logout", "goToIp", "isThereMessageError", {"gotoif":["*.$", "@cleanOwnLogsAgain"]}, "forceToAccessTarget", "signInKnownTarget", "cleanMyIpClues", "isThereMessageError", {"gotoif":["*.$", "@cleanOwnLogsAgain"]}, {"wait": "_forPageToReload"}]},
+	{"@cleanOwnLogsAgain": ["goToOwnLogTab", "cleanTextAreaContent", "isThereMessageError", {"gotoif":["*.$", "@init"]}, {"wait": "_forPageToReload"}, {"gotoif": [1, "@init"]}]},
 	{"@abortProcess": "_exit"}
 ]
-
-/*foo.instructions = [
-	{0: ["startBankCamping", "extractTransferLogAccount", "showResult"]}
-]*/
